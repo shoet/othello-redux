75f0dc220c1cace1ca5dc161543532ea51c593dc
shoet <32211937+shoet@users.noreply.github.com>
2025-03-16 11:00:21 +0900

    GetBoardユースケースを作成する

--- backend/src/usecase/getBoard.ts
+++ backend/src/usecase/getBoard.ts
@@ -1,3 +1,19 @@
-export const getBoardUsecase = async (roomID: string) => {
-  const testdata = [];
-};
+import { Board } from "../domain/board";
+import { BoardID } from "../domain/types";
+
+interface IBoardRepository {
+  getBoard(boardID: BoardID): Promise<Board | undefined>;
+}
+
+export class GetBoardUsecase {
+  private boardRepository: IBoardRepository;
+
+  constructor(boardRepository: IBoardRepository) {
+    this.boardRepository = boardRepository;
+  }
+
+  async run(boardID: BoardID) {
+    const board = await this.boardRepository.getBoard(boardID);
+    return board;
+  }
+}
