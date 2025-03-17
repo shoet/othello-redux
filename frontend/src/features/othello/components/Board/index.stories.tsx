e6166773b11fb31dd280bc26aefd488f372e0fde
shoet <32211937+shoet@users.noreply.github.com>
2025-03-17 22:38:00 +0900

    ボードのUIを実装する

---
+++ frontend/src/features/othello/components/Board/index.stories.tsx
@@ -0,0 +1,98 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Board } from ".";
+import { Cell } from "../../othello";
+
+const cells: Cell[][] = [
+  [
+    { position: { x: 0, y: 0 } },
+    { position: { x: 1, y: 0 } },
+    { position: { x: 2, y: 0 } },
+    { position: { x: 3, y: 0 } },
+    { position: { x: 4, y: 0 } },
+    { position: { x: 5, y: 0 } },
+    { position: { x: 6, y: 0 } },
+    { position: { x: 7, y: 0 } },
+  ],
+  [
+    { position: { x: 0, y: 1 } },
+    { position: { x: 1, y: 1 } },
+    { position: { x: 2, y: 1 } },
+    { position: { x: 3, y: 1 } },
+    { position: { x: 4, y: 1 } },
+    { position: { x: 5, y: 1 } },
+    { position: { x: 6, y: 1 } },
+    { position: { x: 7, y: 1 } },
+  ],
+  [
+    { position: { x: 0, y: 2 } },
+    { position: { x: 1, y: 2 } },
+    { position: { x: 2, y: 2 } },
+    { position: { x: 3, y: 2 } },
+    { position: { x: 4, y: 2 } },
+    { position: { x: 5, y: 2 } },
+    { position: { x: 6, y: 2 } },
+    { position: { x: 7, y: 2 } },
+  ],
+  [
+    { position: { x: 0, y: 3 } },
+    { position: { x: 1, y: 3 } },
+    { position: { x: 2, y: 3 } },
+    { position: { x: 3, y: 3 } },
+    { position: { x: 4, y: 3 } },
+    { position: { x: 5, y: 3 } },
+    { position: { x: 6, y: 3 } },
+    { position: { x: 7, y: 3 } },
+  ],
+  [
+    { position: { x: 0, y: 4 } },
+    { position: { x: 1, y: 4 } },
+    { position: { x: 2, y: 4 } },
+    { position: { x: 3, y: 4 } },
+    { position: { x: 4, y: 4 } },
+    { position: { x: 5, y: 4 } },
+    { position: { x: 6, y: 4 } },
+    { position: { x: 7, y: 4 } },
+  ],
+  [
+    { position: { x: 0, y: 5 } },
+    { position: { x: 1, y: 5 } },
+    { position: { x: 2, y: 5 } },
+    { position: { x: 3, y: 5 } },
+    { position: { x: 4, y: 5 } },
+    { position: { x: 5, y: 5 } },
+    { position: { x: 6, y: 5 } },
+    { position: { x: 7, y: 5 } },
+  ],
+  [
+    { position: { x: 0, y: 6 } },
+    { position: { x: 1, y: 6 } },
+    { position: { x: 2, y: 6 } },
+    { position: { x: 3, y: 6 } },
+    { position: { x: 4, y: 6 } },
+    { position: { x: 5, y: 6 } },
+    { position: { x: 6, y: 6 } },
+    { position: { x: 7, y: 6 } },
+  ],
+  [
+    { position: { x: 0, y: 7 } },
+    { position: { x: 1, y: 7 } },
+    { position: { x: 2, y: 7 } },
+    { position: { x: 3, y: 7 } },
+    { position: { x: 4, y: 7 } },
+    { position: { x: 5, y: 7 } },
+    { position: { x: 6, y: 7 } },
+    { position: { x: 7, y: 7 } },
+  ],
+];
+
+export default {
+  title: "Board",
+  component: Board,
+  args: {
+    cells: cells,
+  },
+} as Meta<typeof Board>;
+
+type Story = StoryObj<typeof Board>;
+
+export const Basic: Story = {};
--- frontend/src/features/othello/components/Board/index.tsx
+++ frontend/src/features/othello/components/Board/index.tsx
@@ -1,3 +1,4 @@
+import { CSSProperties } from "react";
 import { Cell, Cells } from "../../othello";
 import css from "./index.module.scss";
 
@@ -9,26 +10,24 @@ type Props = {
 export const Board = (props: Props) => {
   const { handleClickCell, cells } = props;
 
+  const style = {
+    "--columns": cells.length,
+  } as CSSProperties;
+
   return (
-    <div className={css.board}>
-      <div className={css.column}>
-        {cells.map((row, idx) => {
-          return (
-            <div className={css.row} key={idx}>
-              {row.map((cell, idx) => {
-                return (
-                  <div
-                    className={css.cell}
-                    onClick={() => handleClickCell(cell)}
-                    key={idx}
-                  >
-                    {cell.color}
-                  </div>
-                );
-              })}
+    <div className={css.boardContainer}>
+      <div className={css.board} style={style}>
+        {cells.map((row, rowIdx) =>
+          row.map((cell, colIdx) => (
+            <div
+              className={css.cell}
+              key={`${rowIdx}-${colIdx}`}
+              onClick={() => handleClickCell(cell)}
+            >
+              {cell.color}
             </div>
-          );
-        })}
+          ))
+        )}
       </div>
     </div>
   );
