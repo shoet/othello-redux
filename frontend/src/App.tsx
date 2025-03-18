b420e9eab922d649d57613613faadbb63207392e
shoet <32211937+shoet@users.noreply.github.com>
2025-03-18 09:28:22 +0900

    routeを整備する

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,17 @@
-import { OthelloPage } from "./pages/OthelloPage";
+import { Outlet } from "react-router-dom";
+import { WebSocketContextProvider } from "./features/websocket/components/WebSocketConnectionProvider";
+import { store } from "./store.ts";
+import { Provider as StoreProvider } from "react-redux";
 
 function App() {
-  return <OthelloPage />;
+  const webSocketHost = import.meta.env.VITE_WEB_SOCKET_API_HOST;
+  return (
+    <StoreProvider store={store}>
+      <WebSocketContextProvider host={webSocketHost}>
+        <Outlet />
+      </WebSocketContextProvider>
+    </StoreProvider>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/EntryPage/index.tsx
@@ -0,0 +1,9 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const EntryPage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {}, []);
+  return <div>entry</div>;
+};
