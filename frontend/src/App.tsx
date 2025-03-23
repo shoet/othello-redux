9373e52c24dadef4b578ddc90b8d576827a68969
shoet <32211937+shoet@users.noreply.github.com>
2025-03-23 22:49:59 +0900

    WebSocketConnectionProviderからロジックを分離する

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,18 @@ import { Outlet } from "react-router-dom";
 import { WebSocketContextProvider } from "./features/websocket/components/WebSocketConnectionProvider";
 import { store } from "./store.ts";
 import { Provider as StoreProvider } from "react-redux";
+import { ReceiveWebSocketMessageContainer } from "./components/ReceiveWebSocketMessageContainer/index.tsx";
 
 function App() {
   const webSocketHost = import.meta.env.VITE_WEB_SOCKET_API_HOST;
   return (
-    <StoreProvider store={store}>
-      <WebSocketContextProvider host={webSocketHost}>
-        <Outlet />
-      </WebSocketContextProvider>
-    </StoreProvider>
+    <WebSocketContextProvider host={webSocketHost}>
+      <StoreProvider store={store}>
+        <ReceiveWebSocketMessageContainer>
+          <Outlet />
+        </ReceiveWebSocketMessageContainer>
+      </StoreProvider>
+    </WebSocketContextProvider>
   );
 }
 
---
+++ frontend/src/components/ReceiveWebSocketMessageContainer/index.tsx
@@ -0,0 +1,83 @@
+import { ComponentProps, useEffect } from "react";
+import { useWebSocket } from "../../features/websocket/components/WebSocketConnectionProvider";
+import { useAppDispatch } from "../../hook";
+import { updateProfileAction } from "../../features/websocket/webSocketSlice";
+import { Board, Player } from "../../features/othello/othello";
+import { startGameAction } from "../../features/othello/othelloSlice";
+
+type ReceiveMessagePayload =
+  | { type: "init_profile"; data: { client_id: string } }
+  | { type: "update_profile"; data: { client_id: string; room_id: string } }
+  | { type: "system_message"; data: { message: string } }
+  | {
+      type: "start_game";
+      data: { board: Board; players: Player[]; current_turn_index: 0 };
+    }
+  | { type: "operation"; data: {} };
+
+const tryParseMessage = (
+  message: string
+): ReceiveMessagePayload | undefined => {
+  try {
+    return JSON.parse(message);
+  } catch (e) {
+    console.log("failed to parse request");
+  }
+};
+
+/**
+ * ReceiveWebSocketMessageContainer は、WebSocketから受け取ったメッセージからReduxのActionをdispatchするコンポーネントです。
+ */
+export const ReceiveWebSocketMessageContainer = (
+  props: ComponentProps<"div">
+) => {
+  const { connect, disconnect, registerCallback, removeCallback } =
+    useWebSocket();
+  const dispatch = useAppDispatch();
+
+  const handleReceiveMessage = (message: string) => {
+    const request = tryParseMessage(message);
+    if (!request) {
+      return;
+    }
+    const { type, data } = request;
+    switch (type) {
+      case "init_profile":
+        dispatch(updateProfileAction({ clientID: data.client_id }));
+        break;
+      case "update_profile":
+        if (data.client_id) {
+          dispatch(updateProfileAction({ clientID: data.client_id }));
+        }
+        if (data.room_id) {
+          dispatch(updateProfileAction({ roomID: data.room_id }));
+        }
+        break;
+      case "start_game":
+        dispatch(
+          startGameAction({
+            board: data.board,
+            players: data.players,
+            currentTurnIndex: data.current_turn_index,
+          })
+        );
+        break;
+      case "system_message":
+        console.log(data.message);
+        break;
+      default:
+        console.log("unknown action", type);
+    }
+  };
+
+  useEffect(() => {
+    registerCallback("receive_message", handleReceiveMessage);
+    connect();
+    return () => {
+      removeCallback("receive_message");
+      disconnect();
+    };
+  }, []);
+
+  return <div>{props.children}</div>;
+};
