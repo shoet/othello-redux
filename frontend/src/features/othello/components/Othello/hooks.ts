39fa985e454968dfb92b7e063ef9fc3bc147e59f
shoet <32211937+shoet@users.noreply.github.com>
2025-03-25 09:12:57 +0900

    石をおいたらWebSocketでサーバーに通知する

--- frontend/src/features/othello/components/Othello/hooks.ts
+++ frontend/src/features/othello/components/Othello/hooks.ts
@@ -1,11 +1,7 @@
-import {
-  putCellAction,
-  termAction,
-  reverseCellAction,
-  calcScoreAction,
-} from "../../othelloSlice";
+import { putCellAction } from "../../othelloSlice";
 import { useAppDispatch, useAppSelector } from "../../../../hook";
 import { CellColor, CellPosition } from "../../othello";
+import { useWebSocket } from "../../../websocket/components/WebSocketConnectionProvider";
 
 export const useOthello = () => {
   const dispatch = useAppDispatch();
@@ -13,22 +9,37 @@ export const useOthello = () => {
   const roomID = useAppSelector(
     (state) => state.webSocketReducer.currentRoomID
   );
+  const boardID = useAppSelector((state) => state.othelloReducer.boardID);
   const gameStatus = useAppSelector((state) => state.othelloReducer.status);
   const board = useAppSelector((state) => state.othelloReducer.board);
   const players = useAppSelector((state) => state.othelloReducer.players);
   const currentPlayerIndex = useAppSelector(
     (state) => state.othelloReducer.currentPlayerIndex
   );
+  const { sendCustomMessage } = useWebSocket();
 
-  const handlePutCell = (position: CellPosition, color: CellColor): void => {
-    // 石を配置する
+  const handlePutCell = async (position: CellPosition, color: CellColor) => {
+    // クライアントサイドで石を配置する
     dispatch(putCellAction({ position: position, cellColor: color }));
-    // 石を裏返す
-    dispatch(reverseCellAction({ putedPosition: position, putedColor: color }));
-    // スコアを計算する
-    dispatch(calcScoreAction());
-    // ターンを交代する
-    dispatch(termAction());
+    if (!boardID) {
+      console.error("boardID not found");
+      return;
+    }
+    if (!clientID) {
+      console.error("clientID not found");
+      return;
+    }
+    // サーバーに石の配置を通知する
+    const payload = {
+      type: "operation_put",
+      data: {
+        board_id: boardID,
+        client_id: clientID,
+        position: position,
+        cell_color: color,
+      },
+    };
+    sendCustomMessage(JSON.stringify(payload));
   };
 
   return {
