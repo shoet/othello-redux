5d3cc8d0ee8b7a18af2346a5fb732e067f04371b
shoet <32211937+shoet@users.noreply.github.com>
2025-04-09 22:05:30 +0900

    SQSAdapterを実装する

---
+++ backend/src/infrastracture/adapter/sqsAdapter.ts
@@ -0,0 +1,24 @@
+import { SendMessageCommand, SQSClient } from "@aws-sdk/client-sqs";
+
+export class SQSAdapter {
+  constructor() {}
+
+  async sendMessageFIFO(
+    queueURL: string,
+    messageGroupID: string,
+    body: string
+  ): Promise<void> {
+    const sqsClient = new SQSClient();
+    const sendMessageCommand = new SendMessageCommand({
+      QueueUrl: queueURL,
+      MessageGroupId: messageGroupID,
+      MessageBody: body,
+    });
+    try {
+      await sqsClient.send(sendMessageCommand);
+    } catch (e) {
+      console.error("failed to sendMessage", e);
+      throw new Error("failed to sendMessage");
+    }
+  }
+}
